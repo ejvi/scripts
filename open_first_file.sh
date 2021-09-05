18839fd19b892682dc1f2ddabe0bdbfc0866240c
ejvi <evolutionsup@gmail.com>
2021-09-05 14:49:26 +0300

    Update open_first_file.sh

--- open_first_file.sh
+++ open_first_file.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$(readlink -f "$0")")"
 . ./dir.txt
 cd "$dir"
 shopt -s nullglob
-set -- *mkv
+set -- *
 xdg-open "$1"
 echo -e "\e[42mFILE $1 WAS OPENED\e[0m"
 echo -e "\e[5mPRESS ANY KEY TO EXIT\e[0m"
